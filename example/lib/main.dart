9d543d2541da1fc3e530e519be0df2565e6639df
Gursheesh Singh <singhgursheesh12@gmail.com>
2020-05-27 01:52:20 +0530

    Updating example, adding README, adding image

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,9 @@
+import 'package:credit_card_slider/card_background.dart';
+import 'package:credit_card_slider/card_company.dart';
+import 'package:credit_card_slider/card_network_type.dart';
+import 'package:credit_card_slider/credit_card_slider.dart';
+import 'package:credit_card_slider/credit_card_widget.dart';
+import 'package:credit_card_slider/validity.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,105 +13,98 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+Color kPink = Color(0xFFEE4CBF);
+Color kRed = Color(0xFFFA3754);
+Color kBlue = Color(0xFF4AA3F2);
+Color kPurple = Color(0xFFAF92FB);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class _HomePageState extends State<HomePage> {
+  List<CreditCard> _creditCards = [];
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+  void initState() {
+    super.initState();
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    _creditCards = [
+      CreditCard(
+        cardBackground: SolidColorCardBackground(Colors.black.withOpacity(0.6)),
+        cardNetworkType: CardNetworkType.visaBasic,
+        cardHolderName: 'The boring developer',
+        cardNumber: '1234 1234 1234 1234',
+        company: CardCompany.yesBank,
+        validity: Validity(
+          validThruMonth: 1,
+          validThruYear: 21,
+          validFromMonth: 1,
+          validFromYear: 16,
+        ),
+      ),
+      CreditCard(
+        cardBackground: SolidColorCardBackground(kRed.withOpacity(0.4)),
+        cardNetworkType: CardNetworkType.mastercard,
+        cardHolderName: 'Gursheesh Singh',
+        cardNumber: '2434 2434 **** ****',
+        company: CardCompany.kotak,
+        validity: Validity(
+          validThruMonth: 1,
+          validThruYear: 21,
+        ),
+      ),
+      CreditCard(
+        cardBackground: GradientCardBackground(LinearGradient(
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+          colors: [kBlue, kPurple],
+          stops: [0.3, 0.95],
+        )),
+        cardNetworkType: CardNetworkType.mastercard,
+        cardHolderName: 'Very Very very boring devloper',
+        cardNumber: '4567',
+        company: CardCompany.sbiCard,
+        validity: Validity(
+          validThruMonth: 2,
+          validThruYear: 2021,
+        ),
+      ),
+      CreditCard(
+        cardBackground:
+            ImageCardBackground(AssetImage('images/background_sample.jpg')),
+        cardNetworkType: CardNetworkType.mastercard,
+        cardHolderName: 'John Doe',
+        cardNumber: '2434 2434 **** ****',
+        company: CardCompany.virgin,
+        validity: Validity(
+          validThruMonth: 1,
+          validThruYear: 20,
+        ),
+      ),
+      CreditCard(
+        cardNetworkType: CardNetworkType.rupay,
+        cardHolderName: 'THE BORING DEVELOPER',
+        cardNumber: '2434 2434 **** ****',
+        company: CardCompany.sbi,
+      ),
+    ];
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        child: CreditCardSlider(
+          _creditCards,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
 }
